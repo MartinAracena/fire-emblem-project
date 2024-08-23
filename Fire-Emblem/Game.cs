2e8106da5fde426e3cb20ce559751c37737e13b4
MartinAracena <maaracena@uc.cl>
2024-08-23 03:02:00 -0400

    Added fuction: ReadTeamFiles

--- Fire-Emblem/Game.cs
+++ Fire-Emblem/Game.cs
@@ -2,19 +2,41 @@
 
 namespace Fire_Emblem;
 
-public class Game
-{
+public class Game {
     private View _view;
     private string _teamsFolder;
     
-    public Game(View view, string teamsFolder)
-    {
+    public Game(View view, string teamsFolder) {
         _view = view;
         _teamsFolder = teamsFolder;
     }
 
-    public void Play()
-    {
-        throw new NotImplementedException();
+    public void Play() {
+        ReadTeamsFile();
+    }
+
+    private void ReadTeamsFile() {
+        string[] filePathNames = Directory.GetFiles(_teamsFolder);
+        _view.WriteLine("Elige un archivo para cargar los equipos");
+        for (int i = 0; i < filePathNames.Length; i++) {
+            _view.WriteLine($"{i}: {ReturnFileName(filePathNames[i])}");
+        }
+        int input = int.Parse(_view.ReadLine());
+        string selectedFile = ReturnNormalizedFilePath(filePathNames[input]);
+        DeserializeUnits(selectedFile);
+        
+    }
+    private string ReturnFileName(string filePath) {
+        string fileName = filePath.Split("\\").Last();
+        return fileName;
+    }
+
+    private string ReturnNormalizedFilePath(string filePath) {
+        string[] filePathParts = filePath.Split("\\");
+        return Path.Combine(filePathParts);
+    }
+    private void DeserializeUnits(string fileName) {
+        string jsonString = File.ReadAllText(fileName);
+        Console.WriteLine(jsonString);
     }
 }
--- Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
+++ Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d26c1f047177d1b223c8962187ac5fba38874c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e6cefc30a406a3b629a8b6ff0f8267bd417bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
